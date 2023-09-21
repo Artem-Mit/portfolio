13e2aeb19d1cee27ef55998212e043385eeae8eb
Artem <artem.v.mitin@yandex.ru>
2023-09-22 00:09:48 +0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import { Footer } from '@/components/organisms/Footer/Footer'
 import '../styles/index.scss'
 import { Header } from '@/components/organisms/Header/Header'
-import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: 'Mitin Artem | Frontend developer from Saint.P',
-  description: 'Personal website-portfolio of frontend developer Mitin Artem'
-}
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import { Hero } from "@/components/organisms/Hero/Hero";
 
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Mitin Artem | Frontend developer from Saint.P',
+  description: 'Personal website-portfolio of frontend developer Mitin Artem',
+}
+
 export default function Page() {
   return (
     <Hero />
