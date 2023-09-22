f37d447ed74f27ee30c351bed9d683c8c85d1d54
Artem <artem.v.mitin@yandex.ru>
2023-09-22 13:58:01 +0300

    add adaptive design

--- src/components/organisms/Works/Works.tsx
+++ src/components/organisms/Works/Works.tsx
@@ -8,7 +8,7 @@ import workInProgress from '/public/images/work-progress_5578703.png';
 export const Works: FC = () => {
   return (
     <section className={styles.works}>
-      <SectionTitle header={'Works'} text={`Sorry... This page is still under construction 😒`}/>
+      <SectionTitle header={'Works'} text={`Sorry... This page is still under reconstruction 😒`}/>
       <Image src={workInProgress} width={350} height={350} alt='work-in-progress' className={styles.image} />
     </section>
   )
