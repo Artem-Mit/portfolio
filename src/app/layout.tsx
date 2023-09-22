895ade3187d925ec63d35d4aee7bacd6d4f91765
Artem <artem.v.mitin@yandex.ru>
2023-09-22 14:23:19 +0300

    fix fonts import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
+import { Inter } from 'next/font/google'
 import { Footer } from '@/components/organisms/Footer/Footer'
 import '../styles/index.scss'
 import { Header } from '@/components/organisms/Header/Header'
 
+const inter = Inter({
+  display: 'swap',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800']
+})
 
 export default function RootLayout({
   children,
@@ -9,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <Header />
         <main>
